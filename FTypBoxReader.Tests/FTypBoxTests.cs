4aefa3050ee9d2cecbcb9cff762ab5ac0aa3f601
boegholm <thomas@boegholm.dk>
2022-04-24 07:40:51 +0200

    Handling of malformed headers

--- FTypBoxReader.Tests/FTypBoxTests.cs
+++ FTypBoxReader.Tests/FTypBoxTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,8 +8,28 @@ using Xunit;
 
 namespace FTypBoxReader.Tests
 {
+    static class ByteArrayExtensions
+    {
+        enum Endianness { BigEndian, LittleEndian};
+        public static byte[] ReplaceBigEndianUInt32(this byte[] buf, int index, UInt32 newVal)
+        {
+            var valbuf = BitConverter.GetBytes(newVal);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(valbuf);
+            Array.ConstrainedCopy(valbuf, 0, buf, index, valbuf.Length);
+            return buf;
+        }
+        public static byte[] ReplaceLittleEndianUInt32(this byte[] buf, int index, UInt32 newVal)
+        {
+            var valbuf = BitConverter.GetBytes(newVal);
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(valbuf);
+            Array.ConstrainedCopy(valbuf, 0, buf, index, valbuf.Length);
+            return buf;
+        }
+    }
     // Test images from https://github.com/tigranbs/test-heic-images
-    public class FTypBoxTests
+    public class FTypBoxTestsu
     {
         public static TheoryData<Stream> HeicSources => new()
         {
@@ -17,6 +39,22 @@ namespace FTypBoxReader.Tests
             { new MemoryStream(Resources.image4) },
         };
 
+        public static TheoryData<Stream> MisalignedImages
+            => new()
+        {
+            { MisalignedSize(Resources.image4, -1) },
+            { MisalignedSize(Resources.image4, -2) },
+            { MisalignedSize(Resources.image4, -3) },
+            { MisalignedSize(Resources.image4, 1) },
+            { MisalignedSize(Resources.image4, 2) },
+            { MisalignedSize(Resources.image4, 3) }
+        };
+        private static MemoryStream MisalignedSize(byte[] image, int offset)
+        {
+            var bs = FTypBox.ReadBoxSize(image);
+            return new MemoryStream(image.ReplaceBigEndianUInt32(0, (UInt32)(bs + offset)));
+        }
+
         public static TheoryData<Stream> PngSources => new()
         {
             { new MemoryStream(Resources.image5) },
@@ -24,6 +62,7 @@ namespace FTypBoxReader.Tests
 
         [Theory]
         [MemberData(nameof(HeicSources))]
+        [MemberData(nameof(MisalignedImages))]
         public async Task TryReadFTypBoxAsync_LoadHeicPrimarilyHeic(Stream s)
         {
             var box = await LoadAndClose(s);
@@ -39,6 +78,8 @@ namespace FTypBoxReader.Tests
             Assert.Equal(2, box?.CompatibleBrands.Count());
         }
 
+        
+
         [Theory]
         [MemberData(nameof(HeicSources))]
         public async Task TryReadFTypBoxAsync_CompatibleBrandsContainsMif1(Stream s)
@@ -48,11 +89,36 @@ namespace FTypBoxReader.Tests
         }
         [Theory]
         [MemberData(nameof(HeicSources))]
+        [MemberData(nameof(MisalignedImages))]
         public async Task TestHeicFTypBoxLoad_Success(Stream s)
         {
             var box = await LoadAndClose(s);
             Assert.NotNull(box);
         }
+
+        [Fact]
+        public async Task LoadHugeSizeFTypBoxSizeImage_TruncatedSuccess()
+        {
+            var s = new MemoryStream(Resources.image4.ReplaceBigEndianUInt32(0, UInt32.MaxValue));
+            var box = await LoadAndClose(s);
+            Assert.NotNull(box);
+        }
+
+        [Fact]
+        public async Task LoadZeroFTypBoxSizeImage_Fails()
+        {
+            var box = await LoadAndClose(new MemoryStream(Resources.image1.ReplaceBigEndianUInt32(0, 0)));
+            Assert.Null(box);
+        }
+
+        [Theory]
+        [MemberData(nameof(PngSources))]
+        public async Task TestHeicFTypBoxLoad_LoadUnsuccessful(Stream s)
+        {
+            var box = await LoadAndClose(s);
+            Assert.Null(box);
+        }
+
         private async Task<FTypBox?> LoadAndClose(Stream s)
         {
             try
--- FTypBoxReader/FTypBox.cs
+++ FTypBoxReader/FTypBox.cs
@@ -1,11 +1,12 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace FTypBoxReader
 {
     public class FTypBox
     {
         private List<string> _compatibleBrands = new List<string>();
-        private FTypBox(string type, UInt32 size, Memory<byte> buf)
+        private FTypBox(string type, int size, Memory<byte> buf)
         {
             Type = type;
             Size = size;
@@ -14,7 +15,7 @@ namespace FTypBoxReader
             if (BitConverter.IsLittleEndian)
                 Array.Reverse(versionbuf);
             MinorVersion = BitConverter.ToUInt32(versionbuf);
-            for (int i = 8; i < size; i += 4)
+            for (int i = 8; i < size-FTYP_HEADER_SIZE; i += 4)
             {
                 _compatibleBrands.Add(Encoding.ASCII.GetString(buf.Slice(i, 4).ToArray()));
             }
@@ -25,37 +26,64 @@ namespace FTypBoxReader
 
 
         public string Type { get; }
-        public uint Size { get; }
+        public int Size { get; }
 
         private const int FTYP_HEADER_SIZE= 8;
-        public static async Task<FTypBox?> TryReadFTypBoxAsync(Stream s)
+        private const UInt16 MAX_BOX_SIZE = 128;
+        public static async Task<FTypBox?> TryReadFTypBoxAsync(Stream s, ILogger<FTypBox>? logger=null)
         {
             int k = 0;
             Memory<byte> buf = new byte[FTYP_HEADER_SIZE];
             var rem = buf;
             while (k < FTYP_HEADER_SIZE)
                 k += await s.ReadAsync(rem.Slice(k, FTYP_HEADER_SIZE - k));
-                
-            var size = buf.Slice(0, 4).ToArray();
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(size);
+
+            uint boxSize = ReadBoxSize(buf);
+            if (boxSize <= 16)
+            {  // header+major+minor_version 
+                logger?.LogCritical($"box probably too small to be ftyp (or 1/largesize -- and ignored): {boxSize}");
             }
             var ft = buf.Slice(4, 4);
 
             var typ = Encoding.ASCII.GetString(ft.ToArray());
             if (typ == "ftyp")
             {
-                var remainingBoxSize = (int)BitConverter.ToUInt32(size.ToArray())- FTYP_HEADER_SIZE;
+                int truncatedBoxSize;
+                if (boxSize <= 16)
+                {  // header+major+minor_version 
+                    logger?.LogCritical($"ftyp but box too small: ({boxSize})");
+                    return null;
+                }
+                if (boxSize > MAX_BOX_SIZE)
+                {
+                    truncatedBoxSize = MAX_BOX_SIZE;
+                }
+                else
+                {
+                    truncatedBoxSize = (int)boxSize;
+                }
+                var remainingBoxSize = truncatedBoxSize - FTYP_HEADER_SIZE;
                 Memory<byte> data = new byte[remainingBoxSize];
                 k = 0;
                 while (k < remainingBoxSize)
                 {
                     k += await s.ReadAsync(data.Slice(k, remainingBoxSize - k));
                 }
-                return new FTypBox(typ, (UInt32)remainingBoxSize, data);
+                return new FTypBox(typ, truncatedBoxSize, data);
             }
             else return null;
+        }
+
+        public static UInt32 ReadBoxSize(Memory<byte> buf, ILogger<FTypBox>? logger = null)
+        {
+            var size = buf.Slice(0, 4).ToArray();
+            size[3] &= 0xFC;
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(size);
+            }
+            var boxSize = BitConverter.ToUInt32(size.ToArray()); 
+            return boxSize; // fix alignment
         }
     }
 }
